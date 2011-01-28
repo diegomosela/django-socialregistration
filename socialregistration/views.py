4caddbb3f217f67fd8621e610fb02025ba1d905f
Rodrigo Chacon <rochacon@gmail.com>
2011-01-27 22:13:38 -0200

    Autofill user's first and last name and e-mail for new Twitter users

--- socialregistration/views.py
+++ socialregistration/views.py
@@ -189,6 +189,11 @@ def twitter(request, account_inactive_template='socialregistration/account_inact
     if user is None:
         profile = TwitterProfile(twitter_id=user_info['id'])
         user = User()
+        user_full_name = user_info['name'].split(' ')
+        user.first_name = user_full_name[0]
+        user.last_name = ' '.join(user_full_name[1:])
+        # FIXME Twitter does not provide a way to get the user email address
+        user.email = "%s@twitter" % user_info['screen_name']
         request.session['socialregistration_profile'] = profile
         request.session['socialregistration_user'] = user
         request.session['next'] = _get_next(request)
